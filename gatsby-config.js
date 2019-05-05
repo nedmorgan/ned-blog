8f58d6b2d813b15cf776f2eed01f7209f1e32012
nedmorgan <nmorgan1987@gmail.com>
2019-05-05 19:54:27 -0400

    first commit for developer blog

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+  ],
+}
