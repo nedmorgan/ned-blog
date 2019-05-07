d43dcc05d40907b1cf63846762063d5a7c3f1baa
nedmorgan <nmorgan1987@gmail.com>
2019-05-07 15:56:29 -0400

    created nav bar and added styling to index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,25 +4,54 @@ import styled from "styled-components"
 import { Helmet } from "react-helmet"
 
 const LayoutContainer = styled.div`
-  width: 100%;
+  width: 600px;
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+  margin: 0 auto;
+
+  .nav-link {
+    float: right;
+    margin-left: 1em;
+    margin-top: 3em;
+  }
+
+  img {
+    height: 100px;
+    width: 100px;
+    border-radius: 15px;
+  }
 `
 
 export default ({ children }) => (
-  <LayoutContainer>
-    <Helmet>
-      <title>Ned's Dev Blog</title>
-      <meta
-        name="description"
-        content="A development blog that goes through my journey traversing the world of software development and all the learning that goes with it"
-      />
-      <meta
-        name="keywords"
-        content="development, web, software, engineering, javascript, react, dev, developer"
-      />
-      <meta name="author" content="Ned Morgan" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta charset="UTF-8" />
-      <meta name="referrer" content="origin" />
-    </Helmet>
-  </LayoutContainer>
+  <div>
+    <LayoutContainer>
+      <Helmet>
+        <title>Ned's Dev Blog</title>
+        <meta
+          name="description"
+          content="A development blog that goes through my journey traversing the world of software development and all the learning that goes with it"
+        />
+        <meta
+          name="keywords"
+          content="development, web, software, engineering, javascript, react, dev, developer"
+        />
+        <meta name="author" content="Ned Morgan" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta name="referrer" content="origin" />
+      </Helmet>
+      <div>
+        <Link className={`nav-image`} to={`/`}>
+          <img src={`ned_avatar.png`} alt={`Ned's Avatar`} />
+        </Link>
+        <Link className={`nav-link`} to={`/about/`}>
+          <h3 className={`nav-text`}>About</h3>
+        </Link>
+        <Link className={`nav-link`} to={`mailto:nmorgan1987@gmail.com`}>
+          <h3 className={`nav-text`}>Contact</h3>
+        </Link>
+      </div>
+    </LayoutContainer>
+    {children}
+  </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import styled from "styled-components"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+const IndexContainer = styled.div`
+  font-family: "Lato";
+  font-weight: 400;
+  width: 600px;
+  margin: 0 auto;
+`
+
+export default () => (
+  <Layout>
+    <IndexContainer>
+      <h1>Welcome to My Blog!</h1>
+    </IndexContainer>
+  </Layout>
+)
