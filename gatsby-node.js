3bdddc7406d5637290bcd48c3c36453080edd5ee
nedmorgan <nmorgan1987@gmail.com>
2019-05-10 16:28:55 -0400

    able to create pages and pull readme data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,6 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
@@ -12,6 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
   return graphql(`
     {
       allMarkdownRemark {
@@ -25,6 +28,14 @@ exports.createPages = ({ graphql, actions }) => {
       }
     }
   `).then(result => {
-    console.log(JSON.stringify(result, null, 4))
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
   })
 }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Social from "../components/social"
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <Social />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <p>{post.frontmatter.description}</p>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`
